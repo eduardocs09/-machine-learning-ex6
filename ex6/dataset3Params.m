567a19f3893388be7a373badf0313c628ea99def
Eduardo Silva <eduardo.silva@dtidigital.com.br>
2018-11-27 13:42:24 -0200

    Finished assignemtn

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -22,10 +22,27 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
+min_error = 999999999;
+c_temp = 0;
+sigma_temp = 0;
+example_vet = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
+for c_index = 1:length(example_vet)
+  for sigma_index = 1:length(example_vet)
+    c_i = example_vet(c_index);
+    sigma_i = example_vet(sigma_index);
+    model= svmTrain(X, y, c_i, @(x1, x2) gaussianKernel(x1, x2, sigma_i));
+    predictions = svmPredict(model, Xval);
+    error = mean(double(predictions ~= yval));
+    if (error < min_error)
+      min_error = error;
+      c_temp = c_i;
+      sigma_temp = sigma_i;
+    endif
+  endfor
+endfor
 
-
-
-
+C = c_temp;
+sigma = sigma_temp;
 
 
 
